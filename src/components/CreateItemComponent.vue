775ac732406ce072af2b8536646271b0d0891752
Mihgol <mihgol.js@gmail.com>
2020-02-18 14:26:38 +0100

    added createItem component

---
+++ src/components/CreateItemComponent.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <form v-show="expanded">
+      <div class="form-group">
+        <small class="create text-muted" id="createItemInfo">Create new item</small>
+        <input
+          @keydown.enter="handleAddItem"
+          v-model="title"
+          class="form-control"
+          type="text"
+          id="createItem"
+          aria-describedby="createItemInfo"
+          placeholder="Enter title"
+        />
+
+        <button
+          @click.prevent="handleAddItem"
+          type="submit"
+          class="mt-1 btn btn-primary btn-block"
+          :disabled="title.length === 0"
+        >+</button>
+      </div>
+    </form>
+    <button
+      v-show="!expanded"
+      class="btn btn-secondary mb-3 mt-3 float-right"
+      @click="expanded = true"
+    >+</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "CreateItemComponent",
+  props: {
+    listId: String
+  },
+  data: () => ({
+    title: "",
+    expanded: false
+  }),
+  methods: {
+    handleAddItem(): void {
+      if (this.title) {
+        this.$emit("itemAdded", {
+          listId: this.listId,
+          title: this.title,
+          description: ""
+        });
+        this.title = "";
+        this.expanded = false;
+      }
+    }
+  }
+});
+</script>
+
+
+<style lang="scss">
+.create {
+  padding-left: 1.5px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
-import { BCard } from 'bootstrap-vue'
+import { BootstrapVueIcons } from 'bootstrap-vue'
 
+Vue.use(BootstrapVueIcons)
 Vue.config.productionTip = false
 
 new Vue({
