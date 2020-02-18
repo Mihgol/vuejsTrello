aed09f9c41933d6d2451e15fcd0acb9eb3719b71
Mihgol <mihgol.js@gmail.com>
2020-02-18 12:54:18 +0100

    added board, list components

---
+++ src/components/ListComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+    <h5 class="card-title">{{ list.title }}</h5>
+    <ul class="list-group">
+      <li class="list-group-item" v-for="item in list.items" :key="item.id">{{item.title}}</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Item, List } from "../store/Data";
+import { IAddItem } from "../store/index";
+
+export default Vue.extend({
+  name: "List",
+  props: {
+    list: Object,
+    listIndex: Number
+  }
+});
+</script>
+
+<style lang="scss">
+</style>
